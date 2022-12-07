1a1dfbcb8f56fe8665b36523750d27c6d8c82996
Shave7ma <113388644+Shave7ma@users.noreply.github.com>
2022-12-07 13:46:44 +0300

    Update script5.js

--- script5.js
+++ script5.js
@@ -34,8 +34,10 @@ function calc() {
     p3.value * c3.value +
     p4.value * c4.value +
     p5.value * c5.value;
+  let test=true;
+  if (p0.value<0||p1.value<0||p2.value<0||p3.value<0||p4.value<0||p5.value<0||c0.value<0||c1.value<0||c2.value<0||c3.value<0||c4.value<0||c5.value<0) test=false
   let result = document.getElementById("result");
-  if (isNaN(res)) result.innerHTML = "Что-то пошло не так...";
+  if (isNaN(res)||test==false) result.innerHTML = "Что-то пошло не так...";
   else {
     result.innerHTML = `${name || "Аноним"}, с вас ${res} рублей.`;
   }
