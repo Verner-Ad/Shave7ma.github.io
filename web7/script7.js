22e4b5a03762166f45387217bfc1da13df7da34b
Shave7ma <113388644+Shave7ma@users.noreply.github.com>
2022-12-21 13:48:53 +0300

    Update script7.js

--- web7/script7.js
+++ web7/script7.js
@@ -5,8 +5,6 @@ $(document).ready(function () {
     slidesToShow: 4,
     slidesToScroll: 4,
     dots: true,
-    autoplay: true,
-    autoplaySpeed: 4000,
     responsive: [
       {
         breakpoint: 960,
